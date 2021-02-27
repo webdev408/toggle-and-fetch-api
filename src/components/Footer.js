16cacb062f88e718cf4249b1ade4194b448f6145
Yagya <prolific2476@gmail.com>
2021-02-26 22:04:11 -0500

    added css to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <p className="text-center">2021 &#169; All Rights reserved, www.tSol.com</p>
+            <p>2021 &#169; All Rights reserved, www.tSol.com</p>
         </div>
     )
 }
